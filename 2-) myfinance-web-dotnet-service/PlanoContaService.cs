88db999e5d0db7cd11893fab07e2ebb7d9bb0041
Taila Martins <tailamartins74@gmail.com>
2025-06-05 21:09:53 -0300

    refactor: variaveis de ambiente

--- 2-) myfinance-web-dotnet-service/PlanoContaService.cs
+++ 2-) myfinance-web-dotnet-service/PlanoContaService.cs
@@ -1,4 +1,4 @@
-using System.Security.Cryptography.X509Certificates;
+
 using Microsoft.EntityFrameworkCore;
 using myfinance_web_dotnet_domain.Entities;
 using myfinance_web_dotnet_infra;
--- 4-) myfinance-web-dotnet-infra/MyFinanceDbContext.cs
+++ 4-) myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -1,19 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using myfinance_web_dotnet_domain.Entities;
 
 namespace myfinance_web_dotnet_infra;
 
 public class MyFinanceDbContext : DbContext
+
 {
+    private readonly IConfiguration _configuration;
+
     public DbSet<PlanoConta> PlanoConta { get; set;}
     public DbSet<Transacao> Transacao { get; set;}
 
+    public MyFinanceDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
 
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        //banco de dados local => optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=myfinance;Trusted_Connection=True;Encrypt=False;TrustServerCertificate=False;");
-        optionsBuilder.UseSqlServer(@"Server=meuservidorsqlserver.database.windows.net;Database=myfinance;User Id=User;Password=t.1997.26;");
+        var connectionString = _configuration.GetConnectionString("Database");
+
+        optionsBuilder.UseSqlServer(connectionString);
 
     }
 
