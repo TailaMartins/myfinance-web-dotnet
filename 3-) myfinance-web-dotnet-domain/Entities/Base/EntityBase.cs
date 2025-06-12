780b82aab8600dae7df4f4a82f93340aacb15193
Taila Martins <tailamartins74@gmail.com>
2025-06-12 20:34:00 -0300

    refactor: classes e interfaces genericas

---
+++ 3-) myfinance-web-dotnet-domain/Entities/Base/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace myfinance_web_dotnet_domain.Entities.Base
+{
+    public class EntityBase
+    {
+        [Key]
+        public int? Id { get; set; }
+}
+}
